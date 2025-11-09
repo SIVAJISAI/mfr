39417aaa15b35ec9369622650a8fb6ff40b937c4
Sivaji Sai <sivajisai@Sivajis-MacBook-Pro.local>
2025-11-09 11:55:25 +0530

    used arrow function

--- mfr.js
+++ mfr.js
@@ -1,28 +1,20 @@
 const heading = (text) => console.log("-".repeat(20) + text + "-".repeat(20));
 
-const shorten = (data, method, initialValue) => data.reduce(method, initialValue);
+const blueRibbons = (data) => data.reduce((count, currentRibbon) =>
+  currentRibbon.toLowerCase() === "blue" ? count + 1 : count, 0);
 
-const isBlueRibbon = (count, currentRibbon) => currentRibbon.toLowerCase() === "blue" ? count + 1 : count;
+const uniqueConstellations = (data) => data.flat().reduce(isUnique, []);
 
-const blueRibbons = (data) => shorten(data, isBlueRibbon, 0);
+const uniqueBirds = (data) => data.reduce(isUnique, []);
 
-const filter = (data, method) => data.filter(method);
+const isUnique = (accumalator, item) => {
+  if (!accumalator.includes(item)) {
+    accumalator.push(item);
+  }
+  return accumalator;
+}
 
-const uniqueConstellations = (data) => filter(data.flat(), isUnique);
-
-const isUnique = (element, index, array) => array.indexOf(element) === index;
-
-// const uniqueBirds = (data) => filter(data, isUnique);
-
-const uniqueBirds = (data) => {
-  return data.reduce((accumalator, bird) => {
-    if (!accumalator.includes(bird)) {
-      accumalator.push(bird);
-    }
-    return accumalator;
-  }, []);
-};
-
+const attendance = (data) => data.flat().reduce(isUnique, []);
 
 const areArraysEqual = function (array1, array2) {
   if (array1.length !== array2.length) {
@@ -34,7 +26,6 @@ const areArraysEqual = function (array1, array2) {
       return false;
     }
   }
-
   return true;
 }
 
@@ -64,13 +55,16 @@ const testResults = function (type, description, data, expectedOutput) {
   const actualResult = type(data);
   console.log(formatText(description, data, actualResult, expectedOutput));
 }
+
 const testAttenddance = () => {
   heading("BLUE RIBBONS");
-  testResults(attendance, "at least once", [["Asha", "Ravi", "Neel"],["Ravi"],["Asha", "Meera"]], ["Asha","Ravi","Neel","Meera"])
+  testResults(attendance, "student attended at least once", [["Asha", "Ravi", "Neel"], ["Ravi"], ["Asha", "Meera"]], ["Asha", "Ravi", "Neel", "Meera"]);
+  testResults(attendance, "empty list", [], []);
 }
+
 const testRibbons = () => {
   heading("BLUE RIBBONS");
-  testResults(blueRibbons, "blue ribbons count is 3", ["red", "blue", "blue", "green", "blue", "blue"], 4)
+  testResults(blueRibbons, "blue ribbons count is 3", ["red", "blue", "blue", "green", "blue", "blue"], 4);
   testResults(blueRibbons, "there are no blue ribbons", ["green", "yellow", "red"], 0);
   testResults(blueRibbons, "ribbon is in UPPERCASE", ["BLUE", "blue", "blue", "blue"], 4);
   testResults(blueRibbons, "empty list", [], 0);
@@ -79,9 +73,9 @@ const testRibbons = () => {
 const testUniqueConstellations = () => {
   heading("UNIQUE CONSTELLATIONS");
   testResults(uniqueConstellations, "unique constellations", [["orion", "tautarus"], ["orion", "leo", "gemini"]], ["orion", "tautarus", "leo", "gemini"]);
-  testResults(uniqueConstellations, "same constellation on sasme night", [["orion", "tautarus", "orion"], ["orion", "leo", "gemini"]], ["orion", "tautarus", "leo", "gemini"]);
-
+  testResults(uniqueConstellations, "same constellation on same night", [["orion", "tautarus", "orion"], ["orion", "leo", "gemini"]], ["orion", "tautarus", "leo", "gemini"]);
 }
+
 const testUniqueBirds = () => {
   heading("UNIQUE BIRDS");
   testResults(uniqueBirds, "unique birds", ["sparrow", "crow", "sparrow", "eagle", "crow"], ["sparrow", "crow", "eagle"]);
@@ -92,6 +86,7 @@ const testAll = function () {
   testRibbons();
   testUniqueConstellations();
   testUniqueBirds();
+  testAttenddance();
 
 }
 testAll();
